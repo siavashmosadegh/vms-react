11b3a38ee61fa7c9e4ec1353d09b429ee837ff5a
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-02-09 11:07:12 +0330

    initial setup

---
+++ main.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow, protocol} = require('electron');
+
+const url = require('url');
+
+const path = require('path')
+
+function createMainWindow () {
+    const mainWindow = new BrowserWindow({
+        title: 'electron',
+        width: '1000',
+        height: '600'
+    });
+
+    const startURL = url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protocol: 'file'
+    });
+
+    mainWindow.loadURL(startURL);
+}
+
+app.whenReady().then(createMainWindow);
