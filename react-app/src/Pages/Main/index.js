0fe57f06fdec6ea1aced0ace62250b5edf8956f4
siavashmosadegh <63389544+siavashmosadegh@users.noreply.github.com>
2024-02-09 14:19:59 +0330

    building the UI ...

--- react-app/src/Pages/Main/index.js
+++ react-app/src/Pages/Main/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import {
+    Item,
+    Items,
+    OperationAndControlWrapper,
+    Wrapper
+} from './styles';
 import { Outlet } from 'react-router-dom';
+import MonitorIcon from '../../Icons/MonitorIcon';
+import PlayIcon from '../../Icons/PlayIcon';
+import MapIcon from '../../Icons/MapIcon';
 
 const Main = () => {
     return (
         <Wrapper>
-            <p>ستاد نیرو هوایی</p>
+            <OperationAndControlWrapper>
+                <h3>عملیات و کنترل</h3>
+
+                <Items>
+                    <Item>
+                        <p>دید اصلی</p>
+
+                        <MonitorIcon />
+                    </Item>
+
+                    <Item>
+                        <p>بازبینی</p>
+
+                        <PlayIcon />
+                    </Item>
+
+                    <Item>
+                        <p>نقشه الکترونیکی</p>
+
+                        <MapIcon />
+                    </Item>
+                </Items>
+            </OperationAndControlWrapper>
             <Outlet />
         </Wrapper>
     );
--- react-app/src/Pages/Main/styles.jsx
+++ react-app/src/Pages/Main/styles.jsx
@@ -1,5 +1,41 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
+export const OperationAndControlWrapper = styled.div`
+    width: 80%;
+    background-color: rgb(229,229,229);
+    border-radius: 20px;
+
+    h3 {
+        text-align: right;
+        margin-right: 30px;
+    }
+`;
+
+export const Items = styled.div`
+    margin-right: 20px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    margin-bottom: 12px;
+`;
+
+export const Item = styled.div`
+    border: 1px solid gray;
+    border-radius: 10px;
+    padding-inline: 10px;
+    margin-left: 50px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+        margin-left: 5px;
+    }
 `;
